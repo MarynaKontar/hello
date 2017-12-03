32c6470342806501390bdebe226c100310fe1950
Maryna Kontar <marina.acoustic@gmail.com>
2017-12-03 22:33:12 +0200

    change sayHello method again

--- src/main/java/springboot/heroku/HelloApplication.java
+++ src/main/java/springboot/heroku/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication {
 
 	@GetMapping("/")
 	String sayHello(){
-		return "hello, Marynaaa";
+		return "hello, Marynaaaaaaaaaaaaaaaaaaa";
 	}
 
 	public static void main(String[] args) {
